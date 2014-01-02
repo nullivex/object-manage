f77b5e8b882d9a8195a77b03c1f17b963bc7973f
Bryan Tong <contact@nullivex.com>
2014-01-02 14:31:22 -0700

    reimagined driver format

--- drivers/memory.js
+++ drivers/memory.js
@@ -1,15 +1,19 @@
 'use strict';
 var StorageDriver = require('../lib/StorageDriver')
+  , util = require('util')
 
-var driver = new StorageDriver('memory')
-driver.save(function(handle,data,next){
+var driver = function(options){
+  StorageDriver.call(this,options)
+}
+util.inherits(driver,StorageDriver)
+driver.name = 'memory'
+driver.save = function(handle,data,next){
   next()
-})
-driver.restore(function(handle,next){
+}
+driver.restore = function(handle,next){
   next()
-})
-driver.flush(function(next){
+}
+driver.flush = function(next){
   next()
-})
-
-module.exports = driver
+}
+module.exports = exports = driver
--- drivers/redis.js
+++ drivers/redis.js
@@ -1,29 +1,35 @@
 'use strict';
 var StorageDriver = require('../lib/StorageDriver')
+  , util = require('util')
   , redis = require('redis')
 
-var driver = new StorageDriver('memory')
-driver.setup(function(options){
+var driver = function(options){
+  StorageDriver.call(this,options)
+}
+util.inherits(driver,StorageDriver)
+
+driver.name = 'redis'
+driver.setup = function(options){
   if('object' !== typeof options) options = {}
   var port = options.port || 6379
     , host = options.host || '127.0.0.1'
     , password = options.password || null
   options = options.options || {}
-  driver.client = redis.createClient(port,host,options)
+  driver.handle = redis.createClient(port,host,options)
   if(null !== password){
-    driver.auth(password,function(err){
+    driver.handle.auth(password,function(err){
       if(err) throw err
     })
   }
-})
-driver.save(function(handle,data,next){
-  driver.client.set(handle,data,next)
-})
-driver.restore(function(handle,next){
-  driver.client.get(handle,next)
-})
-driver.flush(function(handle,next){
-  driver.client.set(handle,undefined,next)
-})
+}
+driver.save = function(handle,data,next){
+  driver.handle.set(handle,data,next)
+}
+driver.restore = function(handle,next){
+  driver.handle.get(handle,next)
+}
+driver.flush = function(handle,next){
+  driver.handle.set(handle,undefined,next)
+}
 
-module.exports = driver
+module.exports = exports = driver
